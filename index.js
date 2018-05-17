9e037527170b96d76a8d9609884ae672b934d908
Rolf van der Kaaden <ik@rolfvanderkaaden.nl>
2018-05-17 10:50:40 +0200

    Created NPM package and added iono c lib.

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * -----------------------------------------------------------------------
+ *  Ionopi
+ *  Sferalabs Ionopi Wrapper for NodeJS
+ * -----------------------------------------------------------------------
+ *
+ * @package  ionopi
+ * @author   Rolf van der Kaaden
+ *
+ */
+
+var iono = new function(){
+
+  //reference to self
+  var self = this;
+
+  
+
+}
+
+module.exports = iono;
---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Installing required dependencies...";
+
+cd iono-pi-c-lib
+
+sudo sh build
+
+echo "Done! Test if everything is installed correctly with by using the iono command.";
